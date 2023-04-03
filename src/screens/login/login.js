1c7c94b5461439e89438e684efe5e30395edc94f
Marcin Banaszkiewicz <marcin.banaszkiewicz@300brains.com>
2023-04-03 21:13:59 +0200

    token implementation

--- src/screens/login/login.js
+++ src/screens/login/login.js
@@ -1,16 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Box, Typography, Button, TextField, Container } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Login = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userData, setUserData] = useState({
     userName: "",
     password: "",
   });
 
-  console.log(userData);
+  const [users, setUsers] = useState();
+
+  const [token, setToken] = useState("");
+
+  console.log(token);
+
+  const sendTokenToStorage = (token) => {
+    if (token) {
+      const accessToken = token.access_token;
+      window.localStorage.setItem("TodoToken", JSON.stringify(accessToken));
+    }
+  };
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "http://127.0.0.1:5000/register",
+    })
+      .then((response) => {
+        setUsers(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+  }, []);
+  console.log(users);
 
   const navigation = useNavigate();
 
@@ -25,10 +50,13 @@ const Login = () => {
     })
       .then((response) => {
         console.log(response);
+        setToken(response.data);
+        sendTokenToStorage(token);
       })
       .catch((error) => {
         console.log(error.response);
       });
+
     navigation("/getActions");
   };
 
@@ -48,8 +76,37 @@ const Login = () => {
       });
     navigation("/getActions");
   };
+
+  const deleteUserHandler = async (id) => {
+    console.log("delete");
+    await axios({
+      method: "DELETE",
+      url: `http://localhost:5000/register/${id}`,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const response = await axios({
+      method: "GET",
+      url: "http://127.0.0.1:5000/register",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const updatedResponse = response.data;
+    setUsers(updatedResponse);
+  };
+
   return (
-    <Container sx={{ display: "flex", justifyContent: "center" }}>
+    <Container
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
       <Box
         sx={{
           width: 500,
@@ -92,6 +149,18 @@ const Login = () => {
             <Button onClick={() => LogInHandler()}>Log in</Button>
           )}
         </Box>
+      </Box>
+      <Box>
+        <Typography sx={{ margin: "30px 0" }}>Users</Typography>
+        {users &&
+          users.map((x, idx) => {
+            return (
+              <Box sx={{ display: "flex", flexDirection: "row" }} key={idx}>
+                <Typography>{x.userName}</Typography>
+                <Button onClick={() => deleteUserHandler(x.id)}>X</Button>
+              </Box>
+            );
+          })}
       </Box>
     </Container>
   );
